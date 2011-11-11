419ea03a4e97d06bb6a2f19d24546cc9ff180920
Jordi Bernabeu <jordi.bernabeu@gmail.com>
2011-11-11 21:27:55 +0100

    [customer] Added server-side interaction.

--- android-customer/src/org/catdroid/encaixat/android/customer/ui/ShopActivity.java
+++ android-customer/src/org/catdroid/encaixat/android/customer/ui/ShopActivity.java
@@ -1,6 +1,8 @@
 package org.catdroid.encaixat.android.customer.ui;
 
 import org.catdroid.encaixat.android.customer.R;
+import org.catdroid.encaixat.android.dao.ServerManager;
+import org.catdroid.encaixat.bean.Shop;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -17,15 +19,21 @@ public class ShopActivity extends Activity {
 		
 		// receive shop data from scanner
 		Intent shopData = getIntent();
-		String shopId = shopData.getExtras().getString("SHOP_ID");
+		String idShop = shopData.getExtras().getString("SHOP_ID");
 		
 		// request shop data from server
+		Shop s = null;
+		// Metode que ataca a la BBDD. Això és fake
+		s = ServerManager.sayHello(idShop);
+		// Metode que ataca a la BBDD. Això és fake
+		
 		
 		// show recovered data in views
 		TextView txtShopId = (TextView) findViewById(R.id.txtShopID);
-		txtShopId.setText(shopId);
-		
+		txtShopId.setText(s.getName());
+				
 		TextView txtEncaixada = (TextView) findViewById(R.id.txtEncaixada);
+		txtEncaixada.setText(R.id.txtEncaixada);
 		
 	}
 
