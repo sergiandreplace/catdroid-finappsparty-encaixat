194edb79dcb926547ee37f1286b508d9fdc4ee16
roc boronat <roc.boronat@BCNPO084.blueIT.local>
2011-11-11 17:43:04 +0100

    [server] POJO's created

---
+++ j2ee-server/WEB-INF/src/org/catdroid/encaixat/bean/Customer.java
@@ -0,0 +1,26 @@
+package org.catdroid.encaixat.bean;
+
+public class Customer {
+	String idCustomer;
+	String name;
+	String picture;
+
+	public String getIdCustomer() {
+		return idCustomer;
+	}
+	public void setIdCustomer(String idCustomer) {
+		this.idCustomer = idCustomer;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPicture() {
+		return picture;
+	}
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+}
