779381cb825ac271dfed96301f92e7a0d81ec53d
Sergi Mart?nez <sergi.martinez@gmail.com>
2011-11-11 22:47:52 +0100

    [android-shop] committing layouts

---
+++ android-shop/src/org/catdroid/encaixat/android/shop/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package org.catdroid.encaixat.android.shop.ui;
+
+import org.catdroid.encaixat.android.shop.R;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class MainActivity extends FragmentActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
