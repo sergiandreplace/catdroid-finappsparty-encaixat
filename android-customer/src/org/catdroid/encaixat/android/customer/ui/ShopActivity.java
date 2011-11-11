21ccc5febdd23f35474424692fac2bfb9c9ab840
Jordi Bernabeu <jordi.bernabeu@gmail.com>
2011-11-11 20:39:20 +0100

    [customer] Linking scan result with ShopActivity.

--- android-customer/src/org/catdroid/encaixat/android/customer/ui/ShopActivity.java
+++ android-customer/src/org/catdroid/encaixat/android/customer/ui/ShopActivity.java
@@ -3,7 +3,9 @@ package org.catdroid.encaixat.android.customer.ui;
 import org.catdroid.encaixat.android.customer.R;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ShopActivity extends Activity {
 
@@ -12,6 +14,16 @@ public class ShopActivity extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.shop);
+		
+		// receive shop data from scanner
+		Intent shopData = getIntent();
+		String shopId = shopData.getExtras().getString("SHOP_ID");
+		
+		// request shop data from server
+		
+		// show recovered data in views
+		TextView txtShopId = (TextView) findViewById(R.id.txtShopID);
+		txtShopId.setText(shopId);
 	}
 
 }
