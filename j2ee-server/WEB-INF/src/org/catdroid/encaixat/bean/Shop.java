9a897eff09c50c8b5143334c9dc304f83a9d2731
roc boronat <roc.boronat@BCNPO084.blueIT.local>
2011-11-11 17:43:04 +0100

    [server] POJO's created

---
+++ j2ee-server/WEB-INF/src/org/catdroid/encaixat/bean/Shop.java
@@ -0,0 +1,19 @@
+package org.catdroid.encaixat.bean;
+
+public class Shop {
+	String idShop;
+	String name;
+	
+	public String getIdShop() {
+		return idShop;
+	}
+	public void setIdShop(String idShop) {
+		this.idShop = idShop;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
