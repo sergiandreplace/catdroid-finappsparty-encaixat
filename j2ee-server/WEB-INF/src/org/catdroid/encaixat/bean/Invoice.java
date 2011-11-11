9c21136967b53157c882ebca5b18fb628e5aa2d9
Jordi Bernabeu <jordi.bernabeu@gmail.com>
2011-11-11 18:19:14 +0100

    [customer] Deleting autogenerated code.

---
+++ j2ee-server/WEB-INF/src/org/catdroid/encaixat/bean/Invoice.java
@@ -0,0 +1,26 @@
+package org.catdroid.encaixat.bean;
+
+public class Invoice {
+	String idInvoice;
+	String idShop;
+	String idCustomer;
+	
+	public String getIdInvoice() {
+		return idInvoice;
+	}
+	public void setIdInvoice(String idInvoice) {
+		this.idInvoice = idInvoice;
+	}
+	public String getIdShop() {
+		return idShop;
+	}
+	public void setIdShop(String idShop) {
+		this.idShop = idShop;
+	}
+	public String getIdCustomer() {
+		return idCustomer;
+	}
+	public void setIdCustomer(String idCustomer) {
+		this.idCustomer = idCustomer;
+	}
+}
