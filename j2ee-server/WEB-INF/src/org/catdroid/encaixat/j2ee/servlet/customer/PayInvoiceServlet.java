52835c8062302a4bc6cffd6c018a006268c05fda
Jordi Bernabeu <jordi.bernabeu@gmail.com>
2011-11-11 22:05:28 +0100

    [customer] Added getInvoices stub

---
+++ j2ee-server/WEB-INF/src/org/catdroid/encaixat/j2ee/servlet/customer/PayInvoiceServlet.java
@@ -0,0 +1,59 @@
+package org.catdroid.encaixat.j2ee.servlet.customer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.catdroid.encaixat.bean.Invoice;
+import org.catdroid.encaixat.bean.Transaction;
+import org.catdroid.encaixat.j2ee.bo.SprintBO;
+import org.catdroid.encaixat.j2ee.servlet.BaseHttpServlet;
+import org.catdroid.encaixat.util.Log;
+
+import com.google.gson.Gson;
+
+/** Customer second action: "Hey, what I have to pay today?"
+ * 
+ * @author Catdroid.org - Roc Boronat
+ * @date 11/11/11 19:40
+ */
+@SuppressWarnings("serial")
+public class PayInvoiceServlet extends BaseHttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		String idCustomer = req.getParameter("idCustomer");
+		String idShop = req.getParameter("idShop");
+		String idInvoice = req.getParameter("idInvoice");		
+		
+		if (idCustomer==null){
+			throw new ServletException("idCustomer cannot be null");
+		}
+		if (idShop==null){
+			throw new ServletException("idShop cannot be null");
+		}
+		if (idInvoice==null){
+			throw new ServletException("idInvoice cannot be null");
+		}	
+		
+		Invoice i = null;
+		try {
+			i = SprintBO.payInvoice(idInvoice, idCustomer, idShop);
+		} catch (Exception e) {
+			throw new ServletException(e.getMessage());
+		}
+		
+		prepareHtmlResponse(resp);
+		PrintWriter out = resp.getWriter();
+		out.append(new Gson().toJson(i));
+		out.flush();
+		out.close();
+	}
+
+}
