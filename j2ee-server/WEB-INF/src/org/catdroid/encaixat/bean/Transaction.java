f5a8620cdbf91be56154fdf013fa64ce00473466
Jordi Bernabeu <jordi.bernabeu@gmail.com>
2011-11-11 20:04:34 +0100

    [customer] Integraton with barcode scanner.

---
+++ j2ee-server/WEB-INF/src/org/catdroid/encaixat/bean/Transaction.java
@@ -0,0 +1,32 @@
+package org.catdroid.encaixat.bean;
+
+public class Transaction {
+	Customer customer;
+	Invoice invoice;
+	Shop shop;
+	public Transaction(Customer c, Invoice i, Shop s) {
+		super();
+		this.customer = c;
+		this.invoice = i;
+		this.shop = s;
+	}
+	
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	public Invoice getInvoice() {
+		return invoice;
+	}
+	public void setInvoice(Invoice invoice) {
+		this.invoice = invoice;
+	}
+	public Shop getShop() {
+		return shop;
+	}
+	public void setShop(Shop shop) {
+		this.shop = shop;
+	}
+}
