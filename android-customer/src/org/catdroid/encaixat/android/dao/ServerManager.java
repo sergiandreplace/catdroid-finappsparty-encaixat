dff04b4a92b0f1874c269f828ed931f6a0f1a5ae
Jordi Bernabeu <jordi.bernabeu@gmail.com>
2011-11-11 22:05:28 +0100

    [customer] Added getInvoices stub

--- android-customer/src/org/catdroid/encaixat/android/dao/ServerManager.java
+++ android-customer/src/org/catdroid/encaixat/android/dao/ServerManager.java
@@ -1,9 +1,14 @@
 package org.catdroid.encaixat.android.dao;
 
+import org.catdroid.encaixat.bean.Invoice;
 import org.catdroid.encaixat.bean.Shop;
 
 public class ServerManager {
-	public static Shop sayHello(String idShop){
+	public static Shop sayHello(String idShop) {
 		return new Shop(idShop, "Cansalada Bona");
+	}
+	
+	public static Invoice getInvoices(String idCustomer, String idShop) {
+		return new Invoice("0", idShop, idCustomer, 0.0);
 	}
 }
