565b4d28c31ddc1f4777edc39e4cbebe6128ab5c
Sergi Mart?nez <sergi.martinez@gmail.com>
2011-11-12 01:11:58 +0100

    [android-shop] lists implemented. Ready for server connection

---
+++ android-customer/src/org/catdroid/encaixat/android/customer/ui/ResultActivity.java
@@ -0,0 +1,17 @@
+package org.catdroid.encaixat.android.customer.ui;
+
+import org.catdroid.encaixat.android.customer.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ResultActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.result);
+	}
+
+}
---
+++ android-shop/src/org/catdroid/encaixat/android/shop/adapter/TransactionAdapter.java
@@ -0,0 +1,55 @@
+package org.catdroid.encaixat.android.shop.adapter;
+
+import java.util.ArrayList;
+
+import org.catdroid.encaixat.android.shop.R;
+import org.catdroid.encaixat.bean.Transaction;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class TransactionAdapter extends ArrayAdapter<Transaction> {
+
+	private Context context;
+	private ArrayList<Transaction> transactions;
+
+	public TransactionAdapter(Context context,
+			ArrayList<Transaction> transactions) {
+		super(context, R.layout.transaction_item, transactions);
+		this.context = context;
+		this.transactions = transactions;
+	}
+
+	@Override
+	public View getView(int position, View v, ViewGroup parent) {
+		ViewHolder vh;
+
+		Transaction transaction;
+		if (v == null) {
+            LayoutInflater vi = LayoutInflater.from(context);
+			v = vi.inflate(R.layout.transaction_item, null);
+		}
+		if (v.getTag() == null) {
+			vh = new ViewHolder();
+			vh.name = (TextView) v.findViewById(R.id.TransactionName);
+			v.setTag(vh);
+		} else {
+			vh = (ViewHolder) v.getTag();
+		}
+
+		transaction = transactions.get(position);
+
+		vh.name.setText(transaction.getCustomer().getName());
+
+		return v;
+	}
+
+	private class ViewHolder {
+		TextView name;
+	}
+
+}
