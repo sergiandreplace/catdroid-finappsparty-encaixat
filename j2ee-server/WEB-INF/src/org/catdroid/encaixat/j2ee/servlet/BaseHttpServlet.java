f921697b02b5b61f20e62bf83aa07025f325635d
roc boronat <roc.boronat@BCNPO084.blueIT.local>
2011-11-12 06:23:26 +0100

    [all] Adding quantity to Invoice Bean

---
+++ j2ee-server/WEB-INF/src/org/catdroid/encaixat/j2ee/servlet/BaseHttpServlet.java
@@ -0,0 +1,19 @@
+package org.catdroid.encaixat.j2ee.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
+
+/** Generic class to use JSON Servelts
+ * 
+ * @author Catdroid.org - Roc Boronat
+ * @date 11/11/11 17:48
+ */
+@SuppressWarnings("serial")
+public class BaseHttpServlet extends HttpServlet {
+	protected void prepareJsonResponse(HttpServletResponse resp) {
+		resp.setContentType("application/json; charset=UTF-8");
+	}
+//	protected void prepareHtmlResponse(HttpServletResponse resp) {
+//		resp.setContentType("text/plain; charset=UTF-8");
+//	}	
+}
