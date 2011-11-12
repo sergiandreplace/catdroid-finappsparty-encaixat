959309434acce55066d4f96b1166990460b56f4c
Jordi Bernabeu <jordi.bernabeu@gmail.com>
2011-11-12 04:19:28 +0100

    Combinar

--- android-shop/src/org/catdroid/encaixat/android/shop/ui/MainActivity.java
+++ android-shop/src/org/catdroid/encaixat/android/shop/ui/MainActivity.java
@@ -1,10 +1,14 @@
 package org.catdroid.encaixat.android.shop.ui;
 
+import org.catdroid.encaixat.android.shop.Constants;
 import org.catdroid.encaixat.android.shop.R;
+import org.catdroid.encaixat.android.shop.adapter.TransactionAdapter;
+import org.catdroid.encaixat.android.shop.manager.TransactionManager;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
@@ -12,6 +16,7 @@ import android.widget.ImageView;
 public class MainActivity extends FragmentActivity implements OnClickListener {
 
 	private ImageView buttonRefresh;
+	private MainFragment mainFragment;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -26,16 +31,16 @@ public class MainActivity extends FragmentActivity implements OnClickListener {
 
 	private void getViews() {
 		buttonRefresh = (ImageView) findViewById(R.id.ButtonRefresh);
+		mainFragment = (MainFragment) this.getSupportFragmentManager().findFragmentById(R.id.MainFragment);
 	}
 
 	private void setListeners() {
 		buttonRefresh.setOnClickListener(this);
-		
 
 	}
 
 	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		
+		mainFragment.update();
+
 	}
 }
