0e6efadccdbbb3640d30f0bf9ffc7e471973f573
Jordi Bernabeu <jordi.bernabeu@gmail.com>
2011-11-12 05:54:55 +0100

    [customer] Listener implemented.

---
+++ android-shop/src/org/catdroid/encaixat/android/shop/ui/EnterPriceActivity.java
@@ -0,0 +1,13 @@
+package org.catdroid.encaixat.android.shop.ui;
+
+import org.catdroid.encaixat.android.shop.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EnterPriceActivity extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.enter_quantity);
+	}
+}
