95409f7193565de20c5d3a7dddd96c74544f5b0f
roc boronat <roc.boronat@BCNPO084.blueIT.local>
2011-11-15 23:20:21 +0100

    [all] Updating Android-apps configurations to match production settings. (catdroid.org)

--- android-shop/src/org/catdroid/encaixat/android/shop/Constants.java
+++ android-shop/src/org/catdroid/encaixat/android/shop/Constants.java
@@ -1,7 +1,7 @@
 package org.catdroid.encaixat.android.shop;
 
 public class Constants {
-	public static final String BASEURL="http://172.20.152.19/encaixat/shop/";
+	public static final String BASEURL="http://catdroid.org/encaixat/shop/";
 	public static final String ID_SHOP="0";
 
 }
